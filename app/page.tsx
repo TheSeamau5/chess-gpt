510d6d40be4441df794a1f3a2aa82a3f6fbe234f
Hassan Hayat <hassan.hayat7@gmail.com>
2023-04-03 23:20:57 -0500

    player is white

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 import { Chessboard } from 'react-chessboard'
+import { Chess } from 'chess.js'
+import { useState } from 'react'
 
 export default function Page() {
+    const [game, setGame] = useState(new Chess())
+
     return (
         <div>
-            <Chessboard />
+            <Chessboard
+                position={game.fen()}
+                isDraggablePiece={({ piece }) => piece[0] == 'w'}
+            />
         </div>
     )
 }
