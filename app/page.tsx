dc586e451337f7e813f2ec81a93eb8c62a5266a1
Hassan Hayat <hassan.hayat7@gmail.com>
2023-04-03 23:04:09 -0500

    Render chessboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+'use client'
+import { Chessboard } from 'react-chessboard'
+
 export default function Page() {
-    return <h1 className='text-3xl font-bold underline'>Hello, Next.js!</h1>
+    return (
+        <div>
+            <Chessboard />
+        </div>
+    )
 }
